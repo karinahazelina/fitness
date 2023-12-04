244487cfe15901f45e2f8501cd932ee9fb5efef2
KarinaHazelina <karinagold88@gmail.com>
2023-12-04 21:27:32 +0200

    added animation

--- src/js/script.js
+++ src/js/script.js
@@ -63,4 +63,58 @@ $(document).ready(function () {
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+    function validateForms(formValue){
+        $(formValue).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true 
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите ваше имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите ваш номер телефона",
+                email: {
+                  required: "Пожалуйста, введите вашу эл. почту",
+                  email: "Неправильно введен адрес эл. почты"
+                }
+            }
+        });
+    };
+    
+    validateForms('#consultation-main');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+1 (999) 999-9999");
+
+    // $(this).find("input").val("");
+    // $('#consultation, #order').fadeOut();
+    // $('.overlay, #thanks').fadeIn('slow');
+    // $('form').trigger('reset');
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
